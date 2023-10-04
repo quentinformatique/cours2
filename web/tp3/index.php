2b288a5f368054d6bd6765acbc6c3bb12296cfd6
QUENTIN COSTES <quentin.costes@iut.local>
2023-10-04 10:31:28 +0200

    fin du tp2 de web

--- web/tp3/index.php
+++ web/tp3/index.php
@@ -1,49 +1,47 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>tp3</title>
-    <link rel="stylesheet" href="src/bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="src/css/TP3.css">
-</head>
-<body>
-    <div class="container">
-        <form action="src/php/P1.php"></form>
-        <div class="row">
-            <div class="col-12">
-                <h1>Formulaire</h1>
-            </div>
-            <div class="col-4">
-                <div class="title">Nom :</div>
-                <input type="text" name="nom">
-            </div>
-            <div class="col-4">
-                <div class="title">Prenom :</div>
-                <input type="text" name="nom">
-            </div>
-            <div class="col-4">
-                <div class="title">Diplome préparé :</div>
-                <select name="diplome">
-                    <option value="">--Sélectionnez dans la liste--</option>
-                    <option value="GEA">BUT GEA</option>
-                    <option value="Informatique">BUT Informatique</option>
-                    <option value="QLIO">BUT QLIO</option>
-                    <option value="CJ">BUT CJ</option>
-                    <option value="InfoCom">BU InfoCom</option>
-                </select>
+	<head>
+		<meta charset="utf-8" />
+		<title>TP3</title>
+		<meta name="Description" content="Ma Page De Formulaire">
+		<meta name="Keywords" content="IUT, Rodez">
+		<link rel="stylesheet" href="./TP3.css"/>
+		<link rel='stylesheet' href='./bootstrap-4.6.2-dist/css/bootstrap.css'>
+	</head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Formulaire</h1>
+                </div>
+                <div class="col-4">
+                    <h1>Nom :</h1>
+                    <input type="text" name="Nom" id="Nom"class="form-control">
+                </div>
+                <div class="col-4">
+                    <h1>Prénom :</h1>
+                    <input type="text" name="Prenom" id="Prenom" class="form-control">
+                </div>
+                <div class="col-4">
+                    <h1> Diplôme préparé :</h1>
+                    <input list="Diplome" class="form-control">
 
-            </div>
+                    <datalist id="Diplome">
+                        <option value="Sélectionner dans la liste"></option>
+                        <option value="BUT GEA"></option>
+                        <option value="BUT Informatique"></option>
+                        <option value="BUT QLIO"></option>
+                        <option value="BUT CJ"></option>
+                        <option value="BUT InfoCom"></option>
+                    </datalist>
+                </div>
+                <div class="col-12">
+                    <h1>Votre question :</h1>
+                    <textarea name="Question" id="Question" cols="130" rows="2"></textarea>
+                </div>
             <div class="col-12">
-                <div class="title">Votre question :</div>
-                <textarea name="question" cols="120" rows="1"></textarea>
-            </div>
-            <div class="col-12">
-                <input type="submit" value="Envoyer le formulaire">
+                <button type="button" class="btn btn-light btn-block">Envoyer votre formulaire</button>
             </div>
         </div>
-    </div>
-</body>
+    </body>
 </html>
