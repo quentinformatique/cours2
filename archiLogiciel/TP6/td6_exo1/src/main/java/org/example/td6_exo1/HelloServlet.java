a7f1de900532d3d62546f89cdfbce5afee2cf354
quentinformatique <quentin161204@gmail.com>
2024-03-06 16:00:20 +0100

    architechture logicielle

---
+++ archiLogiciel/TP6/td6_exo1/src/main/java/org/example/td6_exo1/HelloServlet.java
@@ -0,0 +1,27 @@
+package org.example.td6_exo1;
+
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Bonjour</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
