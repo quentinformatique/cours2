abf54565ec7bdf3dacd58a86b57c70876cc28ec3
quentinformatique <quentin161204@gmail.com>
2024-03-19 21:26:04 +0100

    Update ClientServlet.java

--- archiLogiciel/TP8/tp8/src/main/java/Servlets/ClientServlet.java
+++ archiLogiciel/TP8/tp8/src/main/java/Servlets/ClientServlet.java
@@ -1,4 +1,48 @@
 package Servlets;
 
-public class ClientServlet {
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javabeans.Boutique;
+
+import java.io.IOException;
+
+public class ClientServlet extends HttpServlet {
+
+	public void init() {
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		String action = request.getParameter("action");
+		// Retrieve the Boutique instance from the application scope
+		Boutique boutique = (Boutique) getServletContext().getAttribute("boutique");
+		String identifiant = request.getParameter("login");
+		String motDePasse = request.getParameter("password");
+
+		if ("Se connecter".equals(action)) {
+			try {
+				boutique.getClient(identifiant, motDePasse);
+				response.sendRedirect(
+						request.getContextPath() + "/achats.jsp");
+			} catch (Exception e) {
+				response.sendRedirect(
+						request.getContextPath() + "/connexions/connexion_client.jsp");
+			}
+		} else if ("S'inscrire".equals(action)) {
+			try {
+				boutique.enregistrerClient(identifiant, motDePasse);
+				response.sendRedirect(
+						request.getContextPath() + "/clients/achats.jsp");
+			} catch (Exception e) {
+				response.sendRedirect(
+						request.getContextPath() + "/connexions/connexion_client.jsp");
+			}
+		}
+	}
+
+	public void destroy() {
+	}
+
 }
