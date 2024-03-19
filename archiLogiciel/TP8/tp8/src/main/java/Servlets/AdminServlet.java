327a87927c73517d4c74e64cc699e7944f0c91ff
quentinformatique <quentin161204@gmail.com>
2024-03-19 21:26:01 +0100

    Update AdminServlet.java

--- archiLogiciel/TP8/tp8/src/main/java/Servlets/AdminServlet.java
+++ archiLogiciel/TP8/tp8/src/main/java/Servlets/AdminServlet.java
@@ -1,4 +1,26 @@
 package Servlets;
 
-public class AdminServlet {
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "admin-servlet",
+		value = "/admin-servlet")
+
+public class AdminServlet extends HttpServlet {
+
+	public void init() {
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+
+	}
+
+	public void destroy() {
+	}
 }
